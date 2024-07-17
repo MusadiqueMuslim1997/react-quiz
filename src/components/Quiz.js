6f3cc48585c378039629afc4ed8dc2ec9bf66446
MusadiqueMuslim1997 <musadique1997@gmail.com>
2024-07-17 19:54:51 +0500

    add

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -11,7 +11,18 @@ export default function Quiz() {
 <div className="que">
     1. Following data methods can be used to display data in some from using javascript
 </div>
-
+<div className="opt">
+    <input className='rad' type="radio" name="answer" id="option1" />
+    <label htmlFor="option1" id='val1'>document.write()</label>
+</div>
+<div className="opt">
+    <input className='rad' type="radio" name="answer" id="option2" />
+    <label htmlFor="option2" id='val2'>console.log()</label>
+</div>
+<div className="opt">
+    <input className='rad' type="radio" name="answer" id="option3" />
+    <label htmlFor="option3" id='val3'>window.alert()</label>
+</div>
     </div>
 </section>
 
