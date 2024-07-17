c0829b86634c4fffbe9377b84b8344d3906e05c3
MusadiqueMuslim1997 <musadique1997@gmail.com>
2024-07-17 20:27:09 +0500

    add

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import QuizDetail from './components/QuizDetail';
 import Quiz from './components/Quiz';
+import Result from './components/Result';
 function App() {
   return (
     <>
     <QuizDetail/>
     <Quiz/>
+    <Result/>
     </>
   );
 }
